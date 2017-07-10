326d5398d471f21240b69cd8c3ef9f644bc3e2fb
Iván Zaera Avellón <izaera@gmail.com>
2017-07-10 10:58:11 +0200

    Add babel-plugin-shim-nodejs

--- packages/liferay-npm-build-tools-common/src/test.js
+++ packages/liferay-npm-build-tools-common/src/test.js
@@ -2,16 +2,22 @@ import * as babel from 'babel-core';
 
 /**
  * Run a test to transform source code with Babel and match it to a snapshot.
- * @param {Object} plugin the Babel plugin to test
+ * @param {Object|Array} plugin the Babel plugin to test or a valid Babel 
+ *        plugins array
  * @param {String} source the source code to transform
  * @param {Object} options a Babel options hash
  * @return {void}
  */
 export function matchSnapshot(plugin, source, options = {}) {
+	if (!Array.isArray(plugin)) {
+		plugin = [plugin];
+	}
+
 	if (!options.filenameRelative) {
 		options.filenameRelative = __filename;
 	}
-	options.plugins = [plugin];
+
+	options.plugins = plugin;
 
 	const { code } = babel.transform(source, options);
 
